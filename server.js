2604e6f7335d7acd297d848ff615ad75921f9028
Giovanni Bauermeister <giovanni.bauermeister@gmail.com>
2017-06-15 07:21:11 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var request = require('request');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.on('btnClicked', function () {
+    console.log("Button get location clicked!");
+    makeLocationRequest();
+  });
+  //socket.on('accelData', function (data) {
+    //console.log(data);
+  //});
+});
+
+function makeLocationRequest() {
+  request('http://ip-api.com/json/?fields=8405', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body) // print location to console
+      var locationData = body
+      io.emit('locationEvent', locationData);
+    }
+  });
+}
+
+//request('http://ip-api.com/json/?fields=8401', function (error, response, body) {
+//  if (!error && response.statusCode == 200) {
+//    console.log(body) // print location to console
+//    var locationData = body
+//    io.emit('locationEvent', locationData);
+//  }
+//});
+
+app.post('/location', function(req, res) {
+  locationData = req.body
+  io.emit('locationEvent', locationData); 
+});
+
+server.listen(3000, function () {
+  var port = server.address().port
+  console.log('Server listening on mylinkit.local:%s...', port);
+});
